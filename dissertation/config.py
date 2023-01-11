f5840c31fac41b45f9be8c57771faa89cd7552e3
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-01-11 13:44:29 +0100

    add jinja tex pattern

--- dissertation/config.py
+++ dissertation/config.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent
+BUILD_DIR = ROOT_DIR / ".build"
 
 import jinja2
 
---
+++ dissertation/task_process_jinja.py
@@ -0,0 +1,23 @@
+import pytask
+from pathlib import Path
+import toml
+
+from dissertation.config import ROOT_DIR, BUILD_DIR, JINJA_ENV
+
+SYMBOL_INDEX = ROOT_DIR / "symbol_index.tex"
+
+
+@pytask.mark.task(id=str(SYMBOL_INDEX.relative_to(ROOT_DIR)))
+@pytask.mark.depends_on({
+    "template": SYMBOL_INDEX,
+    "data": "symbols.toml"
+})
+@pytask.mark.produces(BUILD_DIR / SYMBOL_INDEX.relative_to(ROOT_DIR))
+def task_process_symbols_index(depends_on: dict[str, Path], produces: Path):
+    template = JINJA_ENV.get_template(str(depends_on["template"].relative_to(ROOT_DIR)))
+
+    symbols = toml.loads(depends_on["data"].read_text())["symbol"]
+
+    result = template.render(symbols=symbols)
+
+    produces.write_text(result)
