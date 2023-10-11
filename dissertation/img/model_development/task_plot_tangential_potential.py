ce5a3da92d6c936360ed776a6b9b684360844c41
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-10-11 11:05:12 +0200

    adapt task_plot_tangential_potential

--- dissertation/img/model_development/task_plot_tangential_potential.py
+++ dissertation/img/model_development/task_plot_tangential_potential.py
@@ -16,7 +16,7 @@ def aup(ds, delta):
 
 
 def alp(ds, delta):
-    return np.sqrt(AL ** 2 + ds ** 2 - 2 * AL * ds * np.cos(delta))
+    return np.sqrt(AL ** 2 + ds ** 2 + 2 * AL * ds * np.cos(delta))
 
 
 def dg(ds, delta, gamma_u, gamma_l):
@@ -24,11 +24,11 @@ def dg(ds, delta, gamma_u, gamma_l):
 
 
 def dg_lin(ds, delta, gamma_u, gamma_l):
-    return -(gamma_u + gamma_l) * np.cos(delta) * ds
+    return -(gamma_u - gamma_l) * np.cos(delta) * ds
 
 
-@pytask.mark.produces([f"plot_normal_potential.{e}" for e in IMAGE_FILE_FORMATS])
-def task_plot_normal_potential(produces: dict[..., Path]):
+@pytask.mark.produces([f"plot_tangential_potential.{e}" for e in IMAGE_FILE_FORMATS])
+def task_plot_tangential_potential(produces: dict[..., Path]):
     fig: plt.Figure = plt.figure(dpi=600)
     ax: plt.Axes = fig.subplots()
 
@@ -39,13 +39,14 @@ def task_plot_normal_potential(produces: dict[..., Path]):
     for i, (gamma_u, gamma_l, delta) in enumerate([
         (1, 1, 60),
         (2, 1, 60),
-        (1, 1, 120),
+        (1, 2, 60),
+        # (2, 1, 120),
     ]):
         handles += ax.plot(
             ds,
             dg(ds, np.deg2rad(delta), gamma_u, gamma_l),
             c=f"C{i}",
-            label=rf"$\Upper\InterfaceEnergy=\qty{{{gamma_u}}}{{\joule\per\square\meter}}, \Lower\InterfaceEnergy=\qty{{{gamma_l}}}{{\joule\per\square\meter}}, \Normal\SurfaceRadiusAngle=\qty{{{delta}}}{{\degree}}$"
+            label=rf"$\Upper\InterfaceEnergy=\qty{{{gamma_u}}}{{\joule\per\square\meter}}, \Lower\InterfaceEnergy=\qty{{{gamma_l}}}{{\joule\per\square\meter}}, \Tangential\SurfaceRadiusAngle=\qty{{{delta}}}{{\degree}}$"
         )
         ax.plot(
             ds,
@@ -56,11 +57,11 @@ def task_plot_normal_potential(produces: dict[..., Path]):
 
     handles+=[
         mlines.Line2D([],[], color="k", ls="-", label="Exakt Solution"),
-        mlines.Line2D([], [], color="k", ls="--", label=r"Tangent in $\Normal\ShiftStep=0$"),
+        mlines.Line2D([], [], color="k", ls="--", label=r"Tangent in $\Tangential\ShiftStep=0$"),
     ]
 
-    ax.set_xlabel(r"$\Normal\ShiftStep$ in \unit{\meter}")
-    ax.set_ylabel(r"$\Normal\GibbsEnergyStep$ in \unit{\joule\per\meter}")
+    ax.set_xlabel(r"$\Tangential\ShiftStep$ in \unit{\meter}")
+    ax.set_ylabel(r"$\Tangential\GibbsEnergyStep$ in \unit{\joule\per\meter}")
     ax.grid(True)
     ax.set_ylim(bottom=1.5 * ax.get_ylim()[0])
 
