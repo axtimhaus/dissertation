19aa860d2341cd306978f1cf8db6fa7e2ffb55b5
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-10-11 10:58:46 +0200

    add task_plot_normal_potential

--- dissertation/config.py
+++ dissertation/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import matplotlib as mpl
 
 ROOT_DIR = Path(__file__).parent
 BUILD_DIR = ROOT_DIR / ".build"
@@ -17,6 +18,13 @@ IMAGE_FILE_FORMATS = [
 ]
 
 IMAGE_FILE_SUFFIXES = ["." + f for f in IMAGE_FILE_FORMATS]
+
+mpl.rcParams.update({
+    "text.usetex": True,
+    "text.latex.preamble": rf"\usepackage{{{ROOT_DIR / 'symbols'}}}\usepackage{{siunitx}}\usepackage{{amsmath}}",
+    "font.family": "serif",
+    "figure.autolayout": True,
+})
 
 import jinja2
 
