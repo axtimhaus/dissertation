97a9f4c9e3137c9629e1034b2d40f98853ab523b
Max Weiner <max.weiner@posteo.de>
2025-05-21 18:45:07 +0200

    update nuget packages and adapt simulation program

--- dissertation/sim/two_particle/Program.cs
+++ dissertation/sim/two_particle/Program.cs
@@ -1,14 +1,14 @@
 using System.Text;
 using System.Text.Json;
-using Microsoft.Extensions.Logging;
 using parameter_study;
 using Plotly.NET;
 using RefraSin.Compaction.ProcessModel;
-using RefraSin.Coordinates;
 using RefraSin.Coordinates.Absolute;
 using RefraSin.MaterialData;
 using RefraSin.ParquetStorage;
+using RefraSin.ParticleModel.Nodes;
 using RefraSin.ParticleModel.ParticleFactories;
+using RefraSin.ParticleModel.Particles;
 using RefraSin.ParticleModel.Remeshing;
 using RefraSin.Plotting;
 using RefraSin.ProcessModel;
@@ -17,6 +17,8 @@ using RefraSin.Storage;
 using RefraSin.TEPSolver;
 using RefraSin.TEPSolver.StepWidthControllers;
 using Serilog;
+
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("run.log").WriteTo.Console().CreateLogger();
 
 var inputFile = args[0];
 var outputFile = args[1];
@@ -85,7 +87,7 @@ var particle2 = new ShapeFunctionParticleFactoryEllipseOvalityCosPeaks(
 
 var initialState = new SystemState(Guid.Empty, 0, [particle1, particle2]);
 
-ParticlePlot.PlotParticles(initialState.Particles).SaveHtml("initialState.html");
+ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(initialState.Particles).SaveHtml("initialState.html");
 
 var compactedState = new FocalCompactionStep(
     new AbsolutePoint(0, 0),
@@ -94,10 +96,7 @@ var compactedState = new FocalCompactionStep(
     maxStepCount: 1
 ).Solve(initialState);
 
-ParticlePlot.PlotParticles(compactedState.Particles).SaveHtml("compactedState.html");
-
-Log.Logger = new LoggerConfiguration().WriteTo.File("run.log").CreateLogger();
-var loggerFactory = LoggerFactory.Create(builder => { builder.AddSerilog(); });
+ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(compactedState.Particles).SaveHtml("compactedState.html");
 
 var routines = SolverRoutines.Default with
 {
@@ -105,11 +104,10 @@ var routines = SolverRoutines.Default with
     StepWidthController = new MaximumDisplacementAngleStepWidthController(maximumDisplacementAngle: input.TimeStepAngleLimit)
 };
 
-var solver = new SinteringSolver(loggerFactory, routines, remeshingEverySteps: 100);
+var solver = new SinteringSolver(routines, remeshingEverySteps: 100);
 
 var plotHandler = new PlotEventHandler();
-
-solver.SessionInitialized += plotHandler.Handle;
+solver.SessionInitialized += plotHandler.HandleSessionInitialized;
 
 var process = new SinteringStep(
     input.Duration,
@@ -124,7 +122,7 @@ process.UseStorage(storage);
 
 var finalState = process.Solve(compactedState);
 
-ParticlePlot.PlotParticles(finalState.Particles).SaveHtml("finalState.html");
+ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(finalState.Particles).SaveHtml("finalState.html");
 
 storage.Dispose();
 Log.CloseAndFlush();
@@ -142,16 +140,27 @@ IEnumerable<IParticleSystemRemesher> YieldRemeshers()
         );
     }
 
-    yield return new NeckNeighborhoodRemesher();
+    yield return new NeckNeighborhoodRemesher(0.4);
 }
 
 class PlotEventHandler
 {
     private int _counter;
 
-    public void Handle(object? sender, SinteringSolver.SessionInitializedEventArgs e)
+    public void HandleSessionInitialized(object? sender, SinteringSolver.SessionInitializedEventArgs e)
     {
-        ParticlePlot.PlotParticles(e.SolverSession.CurrentState.Particles).SaveHtml($"session_{_counter}.html");
+        ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(e.SolverSession.CurrentState.Particles)
+            .SaveHtml($"session_{_counter}.html");
+        _counter++;
+    }
+
+    public void HandleStepCalculated(object? sender, SinteringSolver.StepSuccessfullyCalculatedEventArgs e)
+    {
+        Chart.Combine([
+                ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(e.OldState.Particles),
+                ParticlePlot.PlotParticles<IParticle<IParticleNode>, IParticleNode>(e.NewState.Particles)
+            ])
+            .SaveHtml($"step_{_counter}.html");
         _counter++;
     }
 }
