6aa7ed4d711dade94d352898546041d3af542483
Max Weiner <max.weiner@imf.tu-freiberg.de>
2026-01-02 15:16:06 +0100

    finish slides

--- dissertation/sim/randomized/task_plot_neck_size.py
+++ dissertation/sim/randomized/task_plot_neck_size.py
@@ -8,7 +8,7 @@ from matplotlib.gridspec import GridSpec
 from pytask import mark, task
 
 from dissertation.config import DEFAULT_FIGSIZE, image_produces
-from dissertation.sim.randomized.cases import CASES, MEAN_LINE_STYLE, NOMINAL
+from dissertation.sim.randomized.cases import CASES, MEAN_LINE_STYLE, NOMINAL, SAMPLE_COUNT
 from dissertation.sim.randomized.helper import read_parquet_output_files
 from dissertation.sim.randomized.input import REFERENCE_PARTICLE, TIME_NORM_SURFACE
 
@@ -17,16 +17,16 @@ CUTS = [1e-6, 1e-4]
 CUT_COLORS = ["C2", "C4"]
 
 for case in CASES:
-    for count in [None, 1, 2, 10, 100]:
+    for frame, count in enumerate([SAMPLE_COUNT] + list(np.unique(np.geomspace(1, SAMPLE_COUNT, 50, dtype=int)))):
 
-        @task(id=f"{case.key}{count or ''}")
+        @task(id=f"{case.key}{frame or ''}")
         @mark.plot
         def task_plot_neck_size_randomized(
-            produces=image_produces(case.dir() / f"neck_size{count or ''}"),
+            produces=image_produces(case.dir() / "neck_size" / (f"{frame:02}" if frame else "")),
             results_files=[case.dir(i) / "output.parquet" for i, _ in enumerate(case.samples)][:count],
             nominal_result=NOMINAL.dir() / "output.parquet",
             case=case,
-            alpha=0.7 if count else 0.1,
+            alpha=0.5 - (count / SAMPLE_COUNT) * 0.4,
         ):
             nominal_df = pq.read_table(nominal_result).flatten().flatten()
             data_frames = read_parquet_output_files(results_files)
@@ -83,7 +83,7 @@ for case in CASES:
                 axs[i + 1].axvline(mean - std, color=CUT_COLORS[i], ls="--")
 
             times, values = get_neck_sizes(nominal_df)
-            axs[0].plot(times, values, **NOMINAL.LINE_STYLE, label="nominal")
+            axs[0].plot(times, values, **NOMINAL.LINE_STYLE, label=NOMINAL.display)
 
             for i, v in enumerate(np.interp(t, times, values) for t in CUTS):
                 axs[i + 1].axvline(
@@ -91,7 +91,7 @@ for case in CASES:
                 )
 
             times, values = np.asarray(list(mean_supports.keys())), np.mean(list(mean_supports.values()), axis=-1)
-            axs[0].plot(times, values, **MEAN_LINE_STYLE, label="mean")
+            axs[0].plot(times, values, **MEAN_LINE_STYLE, label="Mean of Samples")
 
             axs[0].set_xlabel("Normalized Time $\\Time / \\TimeNorm_{\\Surface}$")
             axs[0].set_ylabel("Average Relative Neck Size $\\Radius_{\\Neck} / \\Radius_0$")
--- dissertation/sim/randomized/task_plot_shrinkage.py
+++ dissertation/sim/randomized/task_plot_shrinkage.py
@@ -8,7 +8,7 @@ from matplotlib.gridspec import GridSpec
 from pytask import mark, task
 
 from dissertation.config import DEFAULT_FIGSIZE, image_produces
-from dissertation.sim.randomized.cases import CASES, MEAN_LINE_STYLE, NOMINAL
+from dissertation.sim.randomized.cases import CASES, MEAN_LINE_STYLE, NOMINAL, SAMPLE_COUNT
 from dissertation.sim.randomized.helper import read_parquet_output_files
 from dissertation.sim.randomized.input import TIME_NORM_SURFACE, Input
 from dissertation.sim.two_particle.task_plot_shrinkage import distance
@@ -18,16 +18,16 @@ CUTS = [1e-6, 1e-4]
 CUT_COLORS = ["C2", "C4"]
 
 for case in CASES:
-    for count in [None, 1, 2, 10, 100]:
+    for frame, count in enumerate([SAMPLE_COUNT] + list(np.unique(np.geomspace(1, SAMPLE_COUNT, 50, dtype=int)))):
 
-        @task(id=f"{case.key}{count or ''}")
+        @task(id=f"{case.key}{frame or ''}")
         @mark.plot
         def task_plot_shrinkage_randomized(
-            produces=image_produces(case.dir() / f"shrinkage{count or ''}"),
+            produces=image_produces(case.dir() / "shrinkage" / (f"{frame:02}" if frame else "")),
             results_files=[case.dir(i) / "output.parquet" for i, _ in enumerate(case.samples)][:count],
             nominal_result=NOMINAL.dir() / "output.parquet",
             case=case,
-            alpha=0.7 if count else 0.1,
+            alpha=0.5 - (count / SAMPLE_COUNT) * 0.4,
         ):
             nominal_df = pq.read_table(nominal_result).flatten().flatten()
             data_frames = read_parquet_output_files(results_files)
@@ -47,7 +47,7 @@ for case in CASES:
 
             cuts = {t: np.zeros_like(results_files) for t in CUTS}
             mean_supports = {t: np.zeros_like(results_files) for t in np.geomspace(1e-7, 1e-3, 100)}
-            sample_label = "individual samples"
+            sample_label = "Individual Samples"
 
             for i, df in data_frames:
                 sample = case.samples[i]
@@ -85,7 +85,7 @@ for case in CASES:
                 axs[i + 1].axvline(mean - std, color=CUT_COLORS[i], ls="--")
 
             times, values = get_shrinkages_shoelace(NOMINAL.input, nominal_df)
-            axs[0].plot(times, values, **NOMINAL.LINE_STYLE, label="nominal")
+            axs[0].plot(times, values, **NOMINAL.LINE_STYLE, label=NOMINAL.display)
 
             for i, v in enumerate(np.interp(t, times, values) for t in CUTS):
                 axs[i + 1].axvline(
@@ -93,7 +93,7 @@ for case in CASES:
                 )
 
             times, values = np.asarray(list(mean_supports.keys())), np.mean(list(mean_supports.values()), axis=-1)
-            axs[0].plot(times, values, **MEAN_LINE_STYLE, label="mean")
+            axs[0].plot(times, values, **MEAN_LINE_STYLE, label="Mean of Samples")
 
             axs[0].set_xlabel("Normalized Time $\\Time / \\TimeNorm_{\\Surface}$")
             axs[0].set_ylabel("Shrinkage $\\Shrinkage$")
